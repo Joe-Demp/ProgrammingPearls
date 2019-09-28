572131a24913528601c2d71df8c82b91b71d260a
Joe-Demp <dempseyjoseph96@yahoo.com>
2019-09-28 16:25:36 +0100

    Created a class that sorts files of integers using a bitmap

---
+++ PearlsCh1/src/application/BitmapSorter.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import bitmap.IntegerBitmap;
+
+public class BitmapSorter {
+	public static final int N = 10000000;	//	10 million
+	public static final int K = 1000000;	//	1 million
+	
+	public static void sort(String source, String destination) {
+		File in = new File(source);
+		File out = new File(destination);
+		
+		IntegerBitmap bm = new IntegerBitmap(N);
+		
+		try (Scanner scan = new Scanner(in)) {
+			while (scan.hasNextInt()) {
+				bm.add(scan.nextInt());
+			}
+			
+			PrintWriter pw = new PrintWriter(out);
+			Iterator<Integer> it = bm.iterator();
+			while (it.hasNext()) {
+				pw.println(it.next());
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void generateTrivialSource() {
+		try (PrintWriter pw = new PrintWriter("trivial.txt")) {
+			for (int i = K - 1; i >= 0; --i) {
+				pw.println(i);
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}	
+	}
+	
+	public static void main(String[] args) {
+		generateTrivialSource();
+		sort("trivial.txt", "trivial_result.txt");
+	}
+
+}
